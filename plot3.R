7f883a7e32b505644258e927a069f95de7aeca0c
GaborFulop <fulop.gab@gmail.com>
2014-11-09 20:26:52 +0100

    Course project1

---
+++ plot3.R
@@ -0,0 +1,12 @@
+#we need to repeat the previous sequence
+data = read.table("household_power_consumption.txt", sep=";", header=TRUE)
+data$Date = as.Date(data$Date, '%d/%m/%Y')
+data = subset(data, Date == '2007-02-01' | Date == '2007-02-02')
+data = transform(data, Global_active_power = as.numeric(Global_active_power))
+data$Date.Time <- strptime(paste(data$Date, data$Time), format="%Y-%m-%d %H:%M:%S")
+png("plot3.png")
+plot(data$Date.Time, data$Sub_metering_1, type='l', xlab = '', ylab = 'Energy sub metering')
+lines(data$Date.Time, data$Sub_metering_2, type='l', col = "red")
+lines(data$Date.Time, data$Sub_metering_3, type='l', col = "blue")
+legend("topright", lty=1, lwd=1, col=c("black","blue","red"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
